220d7b9d0e0caa7f93da14d6e970668ec189cd00
pong.wu <pong.wu@sunvalley.com.cn>
2020-09-27 10:58:17 +0800

    add chain of responsibility

---
+++ behavioral/chainOfResponsibility/cashier.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type cashier struct {
+	next department
+}
+
+func (c *cashier)execute(p *patient)  {
+	if p.paymentDone{
+		fmt.Println("Payment done")
+	}
+	fmt.Println("Cashier getting money from patient")
+}
+func (c *cashier)setNext(next department)  {
+	c.next = next
+}
